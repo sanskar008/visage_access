a672e107156ea39dec01ec934d4b1e3d69539478
Sanskar Koserwal <sanskark008@gmail.com>
2024-02-12 16:15:54 +0530

    commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Visage Access',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "An application for student's gate in and out records",
+                  textDirection: TextDirection.ltr,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: "Username"),
+                        onChanged: (value) {},
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: "Password"),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/loginpage");
+                        },
+                        child: const Text("Login")),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("Signup"))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:visage_access/homepage.dart';
+import 'package:visage_access/welcomepage.dart';
+import 'package:visage_access/loginpage.dart';
+import 'package:visage_access/signuppage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,73 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        //"/": (context) => const WelcomePage(),
-        //"/login": (context) => const LoginPage(),
-        //"/signup": (context) => const SignupPage(),
-        //"/home": (context) => const HomePage(),
+        "/": (context) => const WelcomePage(),
+        "/login": (context) => const LoginPage(),
+        "/signup": (context) => const SignupPage(),
+        "/home": (context) => const HomePage(),
       },
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Visage Access',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "An application for student's gate in and out records",
-                  textDirection: TextDirection.ltr,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: "Username"),
-                        onChanged: (value) {},
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: "Password"),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, "/loginpage");
-                        },
-                        child: const Text("Login")),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    ElevatedButton(onPressed: () {}, child: Text("Signup"))
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
